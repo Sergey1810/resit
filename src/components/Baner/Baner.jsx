e7c2b98d6eb776f27ddf4998f6134328c80d13b2
Sergey Lisovcev <deadlook15@yandex.ru>
2024-11-23 18:08:10 +0300

    Настройте приложение React для развертывания на GitHub Pages

--- src/components/Baner/Baner.jsx
+++ src/components/Baner/Baner.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Baner.css'
-import logo from "../../images/glav.jpg"
+import logo from "../../images/glav1.png"
 
 const Baner = () => {
     return (
         <div className='baner'>
+            <div className='baner__container'>
             <div className='baner__col'>
-                <h1 className='baner__col-title'>IT - Resurs</h1>
+                <h1 className='baner__col-title'>IT - Ресурс</h1>
                 {/* <p className='baner__col-subtitle'>Сервисный центр, имеющий большой опыт обслуживания техники и программного обеспечения. 
                     Более 22 лет работы в сфере IT принесли множество постоянных клиентов и высокую репутацию, 
                     которую подтверждаем своими качественными результатами работ. 
@@ -15,10 +16,11 @@ const Baner = () => {
                     <p className='baner__col-subtitle'>Компьютерная мастерская</p>
                 {/* <button className='baner__button'>Получить консультацию </button> */}
             </div>
-            <div className='baner__image'>
+            {/* <div className='baner__image'>
+                <div className='baner__image-border'></div>
             <img src={logo} alt="лого" className='baner__image-img'/>
-            </div>
-
+            </div> */}
+</div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import './Navbar.css'
-import { HashLink  } from 'react-router-hash-link';
+import { HashLink } from 'react-router-hash-link';
 import burger from '../../images/burger.png'
 
 const Navbar = () => {
   return (
     <>
-    <ul className='navbar'>
-        <li className='navbar__link' >Портфолио</li>
+      <ul className='navbar'>
         <li className='navbar__link'>Услуги</li>
         <li className='navbar__link'>Цены</li>
+        <li className='navbar__link'>Портфолио</li>
         <li className='navbar__link'>Контакты</li>
         {/* <HashLink to="/home#price">to</HashLink> */}
-       
-    </ul>
-    <img src={burger} alt="меню" className='navbar__burger' />
+
+      </ul>
+      <img src={burger} alt="меню" className='navbar__burger' />
     </>
-    
+
   )
 }
 
