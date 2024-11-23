8b24c70357125e99cc58af751c3a8e71873475f9
Sergey Lisovcev <deadlook15@yandex.ru>
2024-11-23 23:28:07 +0300

    feat:response

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -14,11 +14,18 @@ const Services = () => {
         { id: 2, title: 'Диагностика', price: '500р' },
         { id: 4, title: 'Модернизация', price: '1000р' },
         { id: 5, title: 'Профилактика ', price: '700р' },
-        { id: 7, title: 'Установка/замена комплектующих ', price: '300р' },
-        { id: 8, title: 'Прошивка BIOS материнской платы', price: '700р' },
-        { id: 3, title: 'Оптимизация внутреннего пространства', price: '500р' },
-        { id: 6, title: 'Диагностика отдельных комплектующих ', price: '350р' },
-        { id: 9, title: 'Прошивка BIOS материнской платы на программаторе', price: '1000р' }
+        { id: 6, title: 'Установка/замена комплектующих ', price: '300р' },
+        { id: 7, title: 'Прошивка BIOS материнской платы', price: '700р' },
+        { id: 8, title: '_____________', price: '1000р' },
+        { id: 9, title: 'Диагностика', price: '1000р' }, 
+        { id: 10, title: 'Замена матрицы', price: '1400р' },
+        { id: 11, title: 'Замена разъемов', price: '700р' },
+        { id: 12, title: 'Восстановление Bios', price: '1400р' },
+        { id: 14, title: 'Сборка/разборка ноутбука', price: '1500р' },
+        { id: 13, title: 'Восстановление/ремонт корпуса', price: '2000р' },
+        { id: 15, title: 'Установка/замена комплектующих', price: '1400р' },
+       
+
     ]
 
     const mapService2 = [
@@ -34,62 +41,44 @@ const Services = () => {
        
     ]
     const mapService3 = [
-        { id: 1, title: 'Лазерный принтер, чб, А4', price: '350р' },
-        { id: 2, title: 'Лазерный принтер, чб, А3', price: '700р' },
-        { id: 3, title: 'Лазерный принтер, (цвет), А4', price: '500р' },
-        { id: 4, title: 'Лазерный принтер, (цвет), А3', price: '1000р' },
-        { id: 5, title: 'Копировальный аппарат, А4', price: '350р' },
-        { id: 6, title: 'Копировальный аппарат, А3', price: '500р' },
-        { id: 7, title: 'МФУ лазерный, чб, А4', price: '500р' },
-        { id: 8, title: 'МФУ лазерный, чб, А3', price: '1000р' },
-        { id: 9, title: 'МФУ лазерный, (цвет), А4', price: '700р' },
-        { id: 10, title: 'МФУ лазерный, (цвет), А3', price: '2000р' },
-        { id: 11, title: 'Запуск в эксплуатацию принтера, МФУ, А4', price: '500р' },
-        { id: 12, title: 'Запуск в эксплуатацию принтера, МФУ, А3', price: '700р' },
-        { id: 13, title: 'Прошивка принтера, МФУ', price: '1500р' },
-        { id: 14, title: 'Диагностика принтера, МФУ', price: '500р' },
+        { id: 1, title: 'Ремонт принтера/МФУ', price: '350р' },
+        { id: 2, title: 'Прошивка принтера', price: '700р' },
+        { id: 3, title: 'Техническое обслуживание', price: '500р' },
+        { id: 4, title: 'Заправка картриджей ( HP, Canon, Samsung, Xerox, Brother )', price: '1000р' },
+        { id: 5, title: 'Замена фотобарабана', price: '350р' },
 
-        { id: 15, title: 'Картридж НР', price: '350р-700р' },
-        { id: 16, title: 'Картридж Canon', price: '350р' },
-        { id: 17, title: 'Картридж Samsung', price: '350р' },
-        { id: 18, title: 'Картридж Xerox', price: '700р' },
-        { id: 19, title: 'Картридж Brother', price: '350р-750р' },
-        { id: 20, title: 'Замена фообарабана', price: '550р' },
-        { id: 21, title: 'Замена ролика заряда', price: '150р-350р' },
-        { id: 22, title: 'Замена магнитного вала', price: '300р-400р' },
-        { id: 23, title: 'Замена ракеля', price: '150р-250р' },
+
+       
     ]
     const mapService4 = [
-        { id: 1, title: 'Установка операционной системы Windows', price: '1000р' },
-        { id: 2, title: 'Установка операционной системы Linux', price: '1500р' },
-        { id: 3, title: 'Установка прикладного программного обеспечения', price: '500р' },
-        { id: 4, title: 'Востановление загрузки операционной системы', price: '700р' },
-        { id: 5, title: 'Обновление прикладного программного обеспечения', price: '500р' },
-        { id: 6, title: 'Сохранение перенос данных на резервный накопитель', price: '700р' },
-        { id: 7, title: 'Восстановление данных с носителя', price: '1000р' },
-        { id: 8, title: 'Сброс забытого пароля Windows', price: '500р' },
-        { id: 9, title: 'Установка драйвера на персональный компьютер', price: '500р' },
-        { id: 10, title: 'Удаленная помощь', price: '1200р' },
+        { id: 1, title: 'Установка операционной системы', price: '1000р' },
         { id: 11, title: 'Установка и настройка 1С', price: '2500р' },
-        { id: 12, title: 'Настройка систем ЕГАИС, Честный знак(с лекарствами не работаем)', price: '1500р' },
-        { id: 13, title: 'Корректировка остатков ЕГАИС, ЧЗ', price: '3000р' },
-        { id: 14, title: 'Услуги сопровождения магазина', price: '1500р' },
+        { id: 10, title: 'Удаленная помощь', price: '1200р' },
         { id: 15, title: 'Кассы: продажа, обслуживание, ремонт', price: ' - ' },
-        { id: 16, title: 'Фискальные накопители', price: ' - ' },
-        { id: 17, title: 'Подключение к ОФД', price: ' - ' },
+        { id: 14, title: '______________', price: '1500р' },
+        { id: 3, title: 'Монтаж и ослуживание систем видеонаблюдения', price: '500р' },
+        { id: 4, title: 'Выездное обслуживание/ремонт оборудования', price: '700р' },
+        { id: 6, title: 'Установка и настройка оборудования на выезде', price: '700р' },
+        { id: 5, title: 'Выезд специалиста по заявке', price: '500р' },
+
+      
+        // { id: 7, title: 'Восстановление данных с носителя', price: '1000р' },
+        // { id: 8, title: 'Сброс забытого пароля Windows', price: '500р' },
+        // { id: 9, title: 'Установка драйвера на персональный компьютер', price: '500р' },
+       
+     
+        // { id: 12, title: 'Настройка систем ЕГАИС, Честный знак(с лекарствами не работаем)', price: '1500р' },
+        // { id: 13, title: 'Корректировка остатков ЕГАИС, ЧЗ', price: '3000р' },
+        
     ]
 
     const mapService5 = [
-        { id: 1, title: 'Диагностика неисправности монитора', price: '350р' },
-        { id: 2, title: 'Ремонт монитора', price: '1200р' },
-        { id: 3, title: 'Диагностика неисправности телевизора', price: '1000р' },
-        { id: 4, title: 'Ремонт телевизора (ремонт платы)', price: '2500р' },
-        { id: 5, title: 'Замена подсветки', price: '2000р' },
-        { id: 6, title: 'Диагностика неисправности консоли', price: '1000р' },
-        { id: 7, title: 'Ремонт игровой консоли', price: '2500р' },
-        { id: 8, title: 'Замена дисплея смартфона', price: '3500р' },
-        { id: 9, title: 'Замена разъема/ремонт платы смартфона', price: '1000р' },
-        { id: 10, title: 'Защита гаджетов гидрогелевой пленкой', price: '700р' }
+        { id: 8, title: 'Замена дисплея телефона', price: '3500р' },
+        { id: 9, title: 'Ремонт платы телефона', price: '1000р' },
+        { id: 10, title: 'Гидрогелевая пленка для телефона', price: '700р' },
+        { id: 4, title: 'Ремонт платы телевизора', price: '2500р' },
+        { id: 5, title: 'Замена подсветки телевизора', price: '2000р' },
+        { id: 7, title: 'Чистка/ремонт игровой консоли', price: '2500р' },
     ]
 
     const [service1, setService1] = useState(mapService1)
@@ -152,11 +141,13 @@ const Services = () => {
             </div>
             <div className='service__table'>
                 <div className='service__col-service'>
-                    <div className={`service__col-service-items ${one ? "service__col-service-items_active" : null}`} onClick={onClickService1}>Техническое обслуживание системного блока</div>
-                    <div className={`service__col-service-items ${two ? "service__col-service-items_active" : null}`}  onClick={onClickService2}>Техническое обслуживание ноутбуков</div>
-                    <div className={`service__col-service-items ${three ? "service__col-service-items_active" : null}`}  onClick={onClickService3}>Обслуживание принтеров,МФУ и заправка картриджей </div>
-                    <div className={`service__col-service-items ${four ? "service__col-service-items_active" : null}`}  onClick={onClickService4}>Работа с программным обеспечением и компьютерные услуги</div>
-                    <div className={`service__col-service-items ${five ? "service__col-service-items_active" : null}`}  onClick={onClickService5}>Ремонт прочей техники </div>
+                    <div className={`service__col-service-items ${one ? "service__col-service-items_active" : null}`} onClick={onClickService1}>Техническое обслуживание системного блока/ноутбука</div>
+                    <div className={`service__col-service-items ${five ? "service__col-service-items_active" : null}`}  onClick={onClickService5}>Ремонт техники </div>
+                    <div className={`service__col-service-items ${three ? "service__col-service-items_active" : null}`}  onClick={onClickService3}>Обслуживание оргтехники &nbsp;Заправка картриджей</div>
+                    <div className={`service__col-service-items ${four ? "service__col-service-items_active" : null}`}  onClick={onClickService4}>Компьютерные услуги &nbsp; Обслуживание на выезде</div>
+        
+                    {/* <div className={`service__col-service-items ${four ? "service__col-service-items_active" : null}`}  onClick={onClickService4}>Работа с программным обеспечением и компьютерные услуги</div>
+                     */}
                 </div>
 
                 <div className='service__col2-service'>
